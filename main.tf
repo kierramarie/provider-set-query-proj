fb79f03dde3660f3d1fca194de2d4ebe04f4ef45
kierramarie <kierra.searle@ibm.com>
2026-07-16 15:03:41 -0400

    first commit

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    tfe = {
+      version = "~> 0.79.0"
+    }
+  }
+}
+
+resource "tfe_provider_set" "test1" {
+  name            = "test-set1"
+  description     = "Reusable provider config for selected workspaces"
+  provider_source = "registry.terraform.io/hashicorp/aws"
+  global          = false
+
+  provider_config_hcl = <<-EOT
+  provider "aws" {
+    region = "us-east-1"
+  }
+  EOT
+}
+
+resource "tfe_provider_set" "test2" {
+  name            = "test-set2"
+  description     = "Reusable provider config for selected workspaces"
+  provider_source = "registry.terraform.io/hashicorp/aws"
+  global          = false
+
+  provider_config_hcl = <<-EOT
+  provider "aws" {
+    region = "us-east-1"
+  }
+  EOT
+}
